582b207596b494f5f8b5c25f80cfe47a5c2a05b6
DoseDjalmadaDunhaJunior <djalminhajr_cunha@hotmail.com>
2019-10-26 12:02:25 -0300

    comentado

--- main.cpp
+++ main.cpp
@@ -9,6 +9,7 @@ public:
     char txt[tam];
     char pimm[tam];
     char piN[tam];
+    char T[tam];
     int po;
     int tamanhoT;
     int ip = 0;
@@ -17,6 +18,15 @@ public:
     char* ver;
     bool prio = false;
 
+
+    //força o vetor T ficar com as letras
+    void forca(){
+        for (int i = 65; i < 91; i++) {
+            T[i-65] = i;
+        }
+    }
+
+    //pode apagar a funçao abaixo
     char* imprime(){
         if(true){
 
@@ -28,65 +38,70 @@ public:
         return "t1";
     }
 
+    //empilha os numeros
     void empilhaN(char n) {
         piN[vN] = n;
         vN++;
     }
 
+    //desempilha os pilhas e imprime (tem que arrumar um pouco ainda)
     void desempilhaGeral(){
+
         if(pimm[0] == '+'){
+            tamanhoT++;
+            T[tamanhoT] = T[tamanhoT-1] + 1;
             printf("LDA %c\n"
                    "ADA %c\n"
-                   "STA T%i\n\n", piN[0],piN[1],tamanhoT+1);
+                   "STA %c\n\n", piN[0],piN[1],T[tamanhoT]);
             vN = 0;
             mm--;
         }
         else if(pimm[0] == '-'){
             printf("LDA %c\n"
                    "SUB %c\n"
-                   "STA T%i\n\n", piN[0],piN[1],tamanhoT+1);
+                   "STA %c\n\n", piN[0],piN[1],T[tamanhoT]);
             vN = 0;
             mm--;
         }
         else if((mm == 1) && (pimm[0] == '*')){
             printf("LDA %c\n"
                    "MUL %c\n"
-                   "STA T%i\n\n", piN[0],piN[1],tamanhoT+1);
+                   "STA %c\n\n", piN[0],piN[1],T[tamanhoT]);
             vN = 0;
             mm--;
         }
         else if((mm == 1) && (pimm[0] == '/')){
             printf("LDA %c\n"
                    "DIV %c\n"
-                   "STA T%i\n\n", piN[0],piN[1],tamanhoT+1);
+                   "STA %c\n\n", piN[0],piN[1],T[tamanhoT]);
             vN = 0;
             mm--;
         }
         else if((pimm[0] == '*' || pimm[0] == '/') && (mm > 1) && (pimm[1] == '*')){
 
-        }
+    }
 
     }
 
+    //empilha os sinais
     void empilhaMM(char o) {
         pimm[mm] = o;
         mm++;
     }
 
-    char desempilhaMM() {
-        int a = mm;
-        mm--;
-        imprime();
-        return pimm[a];
-    }
-
-    int conta(char* str,int p,int size) {
+    int conta(char* str,int p,int size,char* algo) {
+        forca();
         if(po == 1){
 
         }
+        for (int i = 0; i < 10; i++) {
+            cout<<algo[i];
+        }
 
+        puts("");
         tamanhoT = size;
         strcpy(txt,str);
+        strcpy(T,algo);
         po = p;
         char ver = txt[po];
 
@@ -126,19 +141,24 @@ public:
 
 private:
 
+    // essa conta no private serve para ficar no loop ate acabar o vetor
     int conta(){
         char ver = txt[po];
+        //aqui identifica quando acaba as chafes e acaba a classe
         if(txt[po] == ')'){
             po++;
         }
 
+        //ele identifica quando a prioridade da pilha é violada ou quando ja tem muito numero (mas aqui ainda esta errado)
         if(!prio && vN == 2){
             desempilhaGeral();
         }
+        //identifica numero
         if ((txt[po] >= '0' && txt[po] <= '9')) {
             empilhaN(txt[po]);
             po++;
         }
+        //identifica vezes ou divide
         else if(txt[po] == '*' || txt[po] == '/'){
             if(mm == 0 || !prio){
                 empilhaMM(txt[po]);
@@ -149,6 +169,7 @@ private:
             }
             po++;
         }
+        //identifica mais ou menos
         else if(txt[po] == '+' || txt[po] == '-'){
             if(mm == 0){
                 prio = false;
@@ -159,15 +180,18 @@ private:
             }
             po++;
         }
+        //identifica quando abre chave e abre uma nova classe
         else if(txt[po] == '('){
             Operacao op;
             po++;
-            op.conta(txt,po,tamanhoT);
+            op.conta(txt,po,tamanhoT,T);
 
         }
+        //identifica quando acaba o vetor
         if(txt[po] == 0){
             desempilhaGeral();
         }
+        //faz a recursao
         if(vN > 0 || mm > 0){
             conta();
         }
@@ -180,7 +204,10 @@ private:
 
 int main() {
     Operacao oi;
+    char y[tam];
     char* tt = "3*(5-2+6-(3*5)+1)-2";
-    oi.conta(tt,0,0);
+    //char* tt = "3*8";
+    oi.conta(tt,0,0,y);
+
     return 0;
 }
